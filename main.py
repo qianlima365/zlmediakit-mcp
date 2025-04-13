e8c4c83058963c4583a5b1c234e81381e51b776c
wangxiaojie <1873297122@qq.com>
2025-04-13 23:55:06 +0800

    dev

--- main.py
+++ main.py
@@ -318,20 +318,20 @@ def create_starlette_app(mcp_server: Server, *, debug: bool = False) -> Starlett
         ],
     )
 
-if __name__ == "__main__":
-    mcp.run(transport='stdio')
-
 # if __name__ == "__main__":
-#     mcp_server = mcp._mcp_server   
+#     mcp.run(transport='stdio')
 
-#     import argparse
+if __name__ == "__main__":
+    mcp_server = mcp._mcp_server   
+
+    import argparse
     
-#     parser = argparse.ArgumentParser(description='Run MCP SSE-based server')
-#     parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
-#     parser.add_argument('--port', type=int, default=8020, help='Port to listen on')
-#     args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Run MCP SSE-based server')
+    parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
+    parser.add_argument('--port', type=int, default=8020, help='Port to listen on')
+    args = parser.parse_args()
 
-#     # Bind SSE request handling to MCP server
-#     starlette_app = create_starlette_app(mcp_server, debug=True)
+    # Bind SSE request handling to MCP server
+    starlette_app = create_starlette_app(mcp_server, debug=True)
 
-#     uvicorn.run(starlette_app, host=args.host, port=args.port)
+    uvicorn.run(starlette_app, host=args.host, port=args.port)
